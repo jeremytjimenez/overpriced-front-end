45aa6ae255a8b815074158e19548e7ab9f8867eb
Irfan Hanjra <ihanjra@gmail.com>
2023-08-10 11:31:24 -0700

    Deployed backend server

--- src/components/Api/Axios.js
+++ src/components/Api/Axios.js
@@ -4,7 +4,7 @@ const AxiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:3001"
-      : "DEPLOY URL",
+      : "https://overpriced-backend.onrender.com",
   timeout: 50000,
 });
 
